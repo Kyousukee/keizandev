c23feeac08b4813f24e725ac29900d61c46f7e45
Kyousukee <38637935+Kyousukee@users.noreply.github.com>
2021-01-20 11:03:37 -0300

    Update enviarcorreo.php

--- enviarcorreo.php
+++ enviarcorreo.php
@@ -25,7 +25,7 @@
 		$mail = @mail($para, $asunto, $mensaje,$header);
 
 		if ($mail) {
-			echo "<script>window.onload=function() {alert('Mensaje enviado');location.replace('http://keizandev.cl/')};</script>";
+			echo "<script>window.onload=function() {location.replace('http://keizandev.cl/');alert('Mensaje enviado');};</script>";
 		}
 	}
 }
