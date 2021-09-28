ccb1f2e74915787bc3bccc8e2c72a39d783ad294
Jose Vergara <jjosemiguel.jv@gmail.com>
2021-09-28 11:54:47 -0300

    imagen

--- Index.php
+++ Index.php
@@ -255,8 +255,7 @@ C# y visual basic. Tambien se reconoce al tener un fuerte conocimiento en base d
 
     </div>
     <div class="col-sm-6">
-      <img src="img/usuario.png" alt="Carlos" style="    width: 150px;
-height: 170px;">
+     
       <img src="img/persona2.jpg" alt="Carlos" style="    width: 150px;
 
     height: 170px;">
